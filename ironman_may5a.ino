56043b6be8257337f3ebf04778b77df3d707c567
Your Name <you@example.com>
2025-05-21 12:28:21 -0700

    Building setup() of voice module and servo control

--- ironman_may5a.ino
+++ ironman_may5a.ino
@@ -9,3 +9,21 @@ DFRobot_DF2301Q_I2C asr;
 #define servo_MAX 450       // Maximum pulse length for servo (180 degrees)
 #define servo_FREQ 50       // Servos run at ~50 Hz
 #define servo_COUNT 10      // Total number of servos
+
+uint8_t servonum = 0;     // Servo counter
+
+void setup() {
+  Serial.begin(115200);
+
+  pwm.begin();
+  pwm.setPWMfreq(50);
+  delay(50);
+
+  while (!(asr.begin())) {
+    Serial.println("Communication with device failed, please check connection.");
+    delay(3000);
+  }
+
+  serial.println("Begin ok!");
+ 
+}
